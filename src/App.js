6030e79f4007450964a29504d75855947f4dcc4f
Srinivas <datlasrinivasvarma@gmail.com>
2025-01-28 12:17:59 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ import Footer from './components/Footer';
 function App() {
   return (
     <DataProvider>
-      <Router>
+      <Router basename="/Atmos">
         <div className='App'>
           <Header />
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path='/Home' element={<Home />} />
-            <Route path='/Others_weather' element={<Others />} />
-            <Route path='/About' element={<About />} />
-            <Route path='*' element={<Error />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Others_weather" element={<Others />} />
+            <Route path="/About" element={<About />} />
+            <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
         </div>
